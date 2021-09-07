fb63a30f0df7e3dfae5470400f0346a54921d243
AbhayMukul <sonumontenegro@gmail.com>
2021-09-07 23:53:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var mysql = require('mysql');
+var cors = require('cors');
+var bodyparser = require('body-parser');
+var app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+app.listen('5000', () => {
+    console.log('server running at port 5000');
+});
+
+var db = mysql.createConnection({
+    host: "database-1.cxaqyyfsqya9.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    password: "admin1234"
+});
+
+// check db connection
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("database conected");
+    }
+})
