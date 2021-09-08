d191fe77e9bb8634dae5329ace611f0a232a77f9
AbhayMukul <sonumontenegro@gmail.com>
2021-09-09 03:11:52 +0530

    unattended graded preparedness attended APIs created

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.listen('5000', () => {
 var db = mysql.createConnection({
     host: "database-1.cxaqyyfsqya9.ap-south-1.rds.amazonaws.com",
     user: "admin",
-    password: "admin1234"
+    password: "admin1234",
+    database: "Trail"
 });
 
 // check db connection
@@ -27,7 +28,61 @@ db.connect((err) => {
 })
 
 app.get('/api', (req, res) => {
-    // console.log(req.query);
-
     res.send("API working")
+})
+
+app.get('/student/AttendedAndGraded', (req, res) => {
+
+    console.log("attended test for id = " + req.body.id);
+
+    let sql = ` SELECT TEST_DETAILS.ID,TEST_DETAILS.NAME,TEST_DETAILS.DESCRIPTION,TEST_DETAILS.DAY,TEST_DETAILS.TIME,TEST_DETAILS.DURATION,TEST_DETAILS.LINK,TEST_STUDENT_DETAILS.STATUS,TEST_STUDENT_DETAILS.SCORE 
+                FROM TEST_DETAILS,TEST_STUDENT_DETAILS
+                WHERE TEST_DETAILS.ID = TEST_STUDENT_DETAILS.TEST_ID
+                AND TEST_STUDENT_DETAILS.STUDENT_ID = ${req.body.id};    
+                `;
+
+    db.query(sql, (err, result) => {
+        if (err) {
+             console.log(err);
+        }
+         res.send(result);
+     })
+})
+
+app.get('/student/Preparedness' , (req,res) => {
+
+    console.log("preparedness for id = " + req.body.id);
+
+    let sql = ` SELECT SUBJECT_DETAILS.ID,SUBJECT_DETAILS.NAME,SUBJECT_DETAILS.CHAPTER,SUBJECT_DETAILS.WEIGHTAGE,STUDENT_PREPAREDNESS.NCERT_TEXTBOOKS,STUDENT_PREPAREDNESS.REFERENCE_BOOKS
+                FROM SUBJECT_DETAILS,STUDENT_PREPAREDNESS
+                WHERE SUBJECT_DETAILS.ID = STUDENT_PREPAREDNESS.CHAPTER_ID
+                AND STUDENT_PREPAREDNESS.STUDENT_ID = ${req.body.id};    
+                `;
+
+
+    db.query(sql, (err, result) => {
+        if (err) {
+             console.log(err);
+        }
+         res.send(result);
+     })
+})
+
+app.get('/student/UnAttended' , (req,res) => {
+
+    console.log("unattended test for id = " + req.body.id);
+
+    let sql = `SELECT * FROM TEST_DETAILS
+                WHERE ID NOT IN 
+                (SELECT ID FROM TEST_STUDENT_DETAILS
+                WHERE TEST_DETAILS.ID = TEST_STUDENT_DETAILS.TEST_ID
+                AND STUDENT_ID = ${req.body.id})
+                `;
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+            res.send(result);
+    })
 })
